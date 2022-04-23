f12e19c2183f57ed806e3ab120cc0f4bf7ecf4ce
WasteLand <karimdzhumakov21082007@gmail.com>
2022-04-23 21:37:25 +0300

    bot invite command

---
+++ commands/invite.js
@@ -0,0 +1,16 @@
+const { SlashCommandBuilder } = require("@discordjs/builders");
+
+// !путь до корневого каталога
+const link = "https://discord.com/api/oauth2/authorize?client_id=702055264324681779&scope=bot&permissions=8";
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("invite")
+        .setDescription("Creates a link to invite a bot."),
+    async execute(interaction) {
+        interaction.reply({
+            content: ("||" + link + "||" + " `-Your link.`" + " `By default, the commands do not work on your server, to activate them write` " + "*`Spirit Of The Hawk#9193`*"),
+            emphemeral: true
+        })
+    }
+};
