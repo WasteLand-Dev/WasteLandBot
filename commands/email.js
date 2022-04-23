f809aa58e2a7f173f67afffc709d3a73b45790ad
WasteLand <karimdzhumakov21082007@gmail.com>
2022-04-23 21:55:07 +0300

    Server info and email

---
+++ commands/email.js
@@ -0,0 +1,13 @@
+const { SlashCommandBuilder } = require("@discordjs/builders");
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("email")
+        .setDescription("Technical support"),
+    async execute(interaction) {
+        interaction.reply({
+            content: "support@wlorigin.cf",
+            emphemeral: true
+        });
+    }
+};
---
+++ commands/serverinfo.js
@@ -0,0 +1,13 @@
+const { SlashCommandBuilder } = require("@discordjs/builders");
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("serverinfo")
+        .setDescription("get information about server"),
+    async execute(interaction) {
+        interaction.reply({
+            content: `Server name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}`,
+            emphemeral: true
+        });
+    }
+};
