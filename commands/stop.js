2130bc6e56a61be7a0877477eb4e18b9d17bc277
WasteLand <karimdzhumakov21082007@gmail.com>
2022-04-23 22:32:28 +0300

    Create stop.js

---
+++ commands/stop.js
@@ -0,0 +1,17 @@
+const { SlashCommandBuilder } = require("@discordjs/builders");
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("stop")
+        .setDescription("stopping the bot"),
+    async execute(interaction) {
+        if (interaction.user.id === '652441677545340928') {
+            process.exit();
+        } else {
+            interaction.reply({
+                content: "You have no right!",
+                emphemeral: true
+            });
+        }
+    }
+};
