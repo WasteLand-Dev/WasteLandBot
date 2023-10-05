4a2f410d6d99ba15587b18673a3378a212f2141f
SpiritOTHawk <Karimdzhumakov21082007@gmail.com>
2023-10-05 14:30:06 +0300

    Update ip.js

--- commands/ip.js
+++ commands/ip.js
@@ -7,7 +7,7 @@ module.exports = {
         .setDescription("Can i connect?"),
     async execute(interaction) {
         interaction.reply({
-            content: "**IP**: radmin.wlorigin.cf / play.wlorigin.cf / craft.wlorigin.cf",
+            content: "**IP**: use ngrok ip / play.wlorigin.cf / craft.wlorigin.cf",
             emphemeral: true
         })
         console.log(color.cyanBright("Command ip executed"));
